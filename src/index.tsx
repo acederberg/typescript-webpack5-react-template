688306617033bd563448f25926bb6d67fe600600
adr1an <adrn.cederberg123@gmail.com>
2021-11-24 10:51:10 -0700

    Battled some annoying webpack issues. It works!

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom' 
+
+function main()
+{
+	console.log( ReactDOM )
+	ReactDOM.render( 
+		<div>It works!</div>,
+		document.getElementById( 'root' )
+	)
+}
+
+main()
