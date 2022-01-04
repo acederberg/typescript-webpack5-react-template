de84d2d140f92f7d0d947913948f4719a22c9f75
adr1an <adrn.cederberg123@gmail.com>
2022-01-03 19:27:42 -0700

    (https://www.youtube.com/watch\?v\=z0rkLRLU6Dw\&list\=PLC3y8-rFHvwiWPS2RO3BKotLRfgg_8WEo\&index\=5)
    Signed-off-by: adr1an <adrn.cederberg123@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser : '@typescript-exlint/parser',
+  parserOptions : {
+    emcaVersion : 2021, // What should this javascript look like, what are the available features
+    sourceType : 'module' // Allow import statements ( not 'requires' )
+  },
+  extends : [
+    // These should have been installed with 'npm' or 'yarn'
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  rules : {
+    // Many of these should already be prevented by the tsconfig, but is here just incase that setting is changed.
+    'no-unused-vars' : 'warn', 
+    '@typescript-eslint/no-unused-vars' : [ 'error' ], 
+    '@typescript-eslint/no-var-requires' : 'warn',
+    'react/prop-types' : 'warn',
+    'react/jsx-uses-react' : 'off', // Do not need to import react
+    'react/react-in-jsx-scope' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types' :'off'
+  },
+  settings : {
+    react : {
+      version : 'detect'
+    }
+  }
+}
